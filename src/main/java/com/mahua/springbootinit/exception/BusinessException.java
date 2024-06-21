9eb8b4f6f5af68827b3cc56a53c431cb94333a96
ma <501847822@qq.com>
2024-06-21 20:50:27 +0800

    模板初始化

---
+++ src/main/java/com/mahua/springbootinit/exception/BusinessException.java
@@ -0,0 +1,40 @@
+package com.mahua.springbootinit.exception;
+
+
+import com.mahua.springbootinit.common.ErrorCode;
+
+/**
+ * 自定义业务异常类
+ *
+ * mahua
+ */
+public class BusinessException extends RuntimeException {
+
+	private final int code;
+	private String description;
+
+	public BusinessException(int code, String msg, String description) {
+		super(msg);
+		this.code = code;
+		this.description = description;
+	}
+
+	public BusinessException(ErrorCode errorCode) {
+		super(errorCode.getMessage());
+		this.code = errorCode.getCode();
+	}
+
+	public BusinessException(ErrorCode errorCode, String description) {
+		super(errorCode.getMessage());
+		this.code = errorCode.getCode();
+		this.description = description;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+}
---
+++ src/test/java/com/mahua/springbootinit/SpringBootInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahua.springbootinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
